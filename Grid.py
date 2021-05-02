b404fad6b3a3b9be9e62a9ca00249c798758a4d9
Adam Dormier <adam@recroom.com>
2021-05-02 14:00:14 -0700

    Initial commit

---
+++ Grid.py
@@ -0,0 +1,185 @@
+from enum import Enum
+import random
+import copy
+from typing import List, Tuple
+
+
+class State(Enum):
+    UNKNOWN = 0
+    RED = 1
+    BLUE = 2
+
+    def __str__(self):
+        if self.value == 0:
+            return 'O'
+        elif self.value == 1:
+            return '\U0001f7e5'
+        else:
+            return '\U0001f7e6'
+
+    def __repr__(self):
+        return self.name
+
+class ContradictionException(Exception):
+    pass
+
+class Grid:
+
+    def __init__(self, row_clues: List[List[int]], col_clues: List[List[int]], grid = None):
+        self.row_clues = row_clues
+        self.col_clues = col_clues
+        self.nRows = len(self.row_clues)
+        self.nCols = len(self.col_clues)
+
+        self.grid: List[List[State]] = [[State.UNKNOWN for y in range(self.nCols)] for x in range(self.nRows)] \
+            if grid is None else grid
+
+    @staticmethod
+    def from_puzzle_num(puzzle_num: int):
+        row_file = f'rows_{puzzle_num}'
+        col_file = f'cols_{puzzle_num}'
+        row_clues = Grid.parse_clues(row_file)
+        col_clues = Grid.parse_clues(col_file)
+
+        return Grid(row_clues, col_clues)
+
+    def __str__(self):
+        return '\n'.join(''.join(str(x) for x in row) for row in self.grid)
+
+    def solve(self):
+        dirty = True
+        iteration = 0
+        while dirty:
+            dirty = False
+            for row_num in range(self.nRows):
+                new_row, new_dirty = self.solveRow(self.getRow(row_num), self.row_clues[row_num], fill_color=State.RED)
+                if new_dirty:
+                    dirty = True
+                    self.setRow(row_num, new_row)
+
+            for col_num in range(self.nCols):
+                new_col, new_dirty = self.solveRow(self.getCol(col_num), self.col_clues[col_num], fill_color=State.BLUE)
+                if new_dirty:
+                    dirty = True
+                    self.setCol(col_num, new_col)
+
+            iteration += 1
+
+        unknowns = self.get_unknown_coords()
+        if unknowns:
+            cached_grid = copy.deepcopy(self.grid)
+            spec_row, spec_col = random.choice(unknowns)
+            print(f'Speculating ({spec_row, spec_col} is RED')
+            try:
+                self.grid[spec_row][spec_col] = State.RED
+                self.solve()
+            except ContradictionException:
+                self.grid = cached_grid
+                try:
+                    print(f'Got contradiction! Try ({spec_row, spec_col} is BLUE')
+                    self.grid[spec_row][spec_col] = State.BLUE
+                    self.solve()
+                except ContradictionException:
+                    self.grid = cached_grid
+                    print(f'({spec_row}, {spec_col}) is a contradiction. Back up.')
+                    raise
+
+
+
+    def get_unknown_coords(self) -> List[Tuple[int, int]]:
+        result = []
+        for row_num, row in enumerate(self.grid):
+            result += ((row_num, col_num) for col_num, col in enumerate(row) if col == State.UNKNOWN)
+        return result
+
+    def getRow(self, row_num: int):
+        return self.grid[row_num].copy()
+
+    def setRow(self, row_num: int, new_row: List[State]):
+        self.grid[row_num] = new_row
+
+    def getCol(self, col_num):
+        return [row[col_num] for row in self.grid]
+
+    def setCol(self, col_num, new_col: List[State]):
+        for i, x in enumerate(new_col):
+            self.grid[i][col_num] = x
+
+    def solveRow(self, row: List[State], clues: List[int], fill_color: State) -> Tuple[List[State], bool]:
+        x_color = State.RED if fill_color == State.BLUE else State.BLUE
+        dirty = False
+
+        def helper(clue_index):
+            nonlocal row
+            nonlocal dirty
+            left = next(i for i, x in enumerate(row) if x != x_color)
+            right = len(row) - next(i for i, x in enumerate(reversed(row)) if x != x_color)
+            for clue in clues[:clue_index]:
+                while any(x == x_color for x in row[left:left+clue]):
+                    left += 1
+                left += clue + 1
+            for clue in clues[-1:clue_index:-1]:
+                while any(x == x_color for x in row[right - clue:right]):
+                    right -= 1
+                right -= clue + 1
+
+
+            clue = clues[clue_index]
+            while any(x == x_color for x in row[left:left+clue]):
+                left += 1
+            while any(x == x_color for x in row[right - clue:right]):
+                right -= 1
+
+
+            width = right - left
+            if width < clue:
+                raise ContradictionException('Contradiction!')
+            if clue == width:
+                if row[left:right] != [fill_color] * width:
+                    dirty = True
+                    row[left:right] = [fill_color] * width
+                if left > 1:
+                    if row[left-1] != x_color:
+                        dirty = True
+                        row[left-1] = x_color
+                if right < len(row) - 2:
+                    if row[right] != x_color:
+                        dirty = True
+                        row[right] = x_color
+            elif clue * 2 > width:
+                e = width - clue
+                new_fill = [fill_color] * (right - left - 2*e)
+                if row[left+e:right-e] != new_fill:
+                    dirty = True
+                    row[left+e:right-e] = new_fill
+
+        for i in range(len(clues)):
+            helper(i)
+
+        return row, dirty
+
+    @staticmethod
+    def parse_clues(filename):
+        clues = []
+        with open(filename, 'r') as f:
+            for line in f:
+                clues.append(list(map(int, line.strip().split())))
+        return clues
+
+if __name__ == '__main__':
+    puzzle_num = int(input('Puzzle Number: '))
+    row_file = f'rows_{puzzle_num}'
+    col_file = f'cols_{puzzle_num}'
+
+    # Read row hints
+    row_clues = []
+    with open(row_file, 'r') as f:
+        for line in f:
+            row_clues.append(list(map(int, line.strip().split())))
+
+    # Read col hints
+    col_clues = []
+    with open(col_file, 'r') as f:
+        for line in f:
+            col_clues.append(list(map(int, line.strip().split())))
+
